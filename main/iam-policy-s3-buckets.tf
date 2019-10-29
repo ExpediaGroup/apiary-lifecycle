9a93085cd4639849a487518aa423d27db965c319
Georgi Ivanov <givanov@hotels.com>
2019-10-29 17:27:36 +0000

    updating the beekeeper cleanup iam role

--- main/iam-policy-s3-buckets.tf
+++ main/iam-policy-s3-buckets.tf
@@ -4,26 +4,6 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
-data "template_file" "beekeeper_s3_allowed_buckets" {
-  template = <<JSON
-$${val}
-JSON
-
-  vars {
-    val = "${jsonencode(
-              distinct(
-                    compact(
-                        split(",",
-                                contains(var.allowed_s3_buckets, "*") || length(compact(var.allowed_s3_buckets)) == 0
-                                ? "*"
-                                : join(",", concat(
-                                    formatlist("%s%s", "arn:aws:s3:::", var.allowed_s3_buckets),
-                                    formatlist("%s%s/*", "arn:aws:s3:::", var.allowed_s3_buckets)
-                                  ))
-                            ))))}"
-  }
-}
-
 resource "aws_iam_policy" "beekeeper_s3" {
   name = "${local.instance_alias}-s3"
 
@@ -39,7 +19,15 @@ resource "aws_iam_policy" "beekeeper_s3" {
         "s3:DeleteObject",
         "s3:DeleteObjectVersion"
       ],
-      "Resource": "${data.template_file.beekeeper_s3_allowed_buckets.rendered}"
+      "Resource": "${jsonencode(distinct(compact(
+                        split(",",
+                                contains(var.allowed_s3_buckets, "*") || length(compact(var.allowed_s3_buckets)) == 0
+                                ? "*"
+                                : join(",", concat(
+                                    formatlist("%s%s", "arn:aws:s3:::", var.allowed_s3_buckets),
+                                    formatlist("%s%s/*", "arn:aws:s3:::", var.allowed_s3_buckets)
+                                  ))
+                            ))))}"
     }
   ]
 }
