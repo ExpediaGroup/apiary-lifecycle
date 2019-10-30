381865564c176e70be8acf776a0d8924cf0691fd
Georgi Ivanov <givanov@hotels.com>
2019-10-30 15:05:12 +0000

    updating the beekeeper cleanup iam role

--- main/iam-policy-s3-buckets.tf
+++ main/iam-policy-s3-buckets.tf
@@ -19,11 +19,7 @@ resource "aws_iam_policy" "beekeeper_s3" {
         "s3:DeleteObject",
         "s3:DeleteObjectVersion"
       ],
-      "Resource": [
-                    "${join("\",\"", formatlist("arn:aws:s3:::%s",var.allowed_s3_buckets))}",
-                    "${join("\",\"", formatlist("arn:aws:s3:::%s/*",var.allowed_s3_buckets))}"
-                  ] 
-                
+      "Resource": ["*"] 
     }
   ]
 }
