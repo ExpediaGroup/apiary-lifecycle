8022d070ab3cc3a229b371c29f1c4d4727bdce8d
max-jacobs <mjacobs@hotels.com>
2019-10-30 10:40:31 +0000

    Trying to fix indexes

--- main/iam.tf
+++ main/iam.tf
@@ -28,13 +28,13 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "beekeeper_ecs_task_exec" {
   count      = "${var.instance_type == "ecs" ? 1 : 0}"
-  role       = "${aws_iam_role.beekeeper_ecs_task_exec.id[0]}"
+  role       = "${aws_iam_role.beekeeper_ecs_task_exec.*.id[0]}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "beekeeper_ecs_task_docker_secrets" {
   count      = "${var.docker_registry_auth_secret_name != "" ? 1 : 0}"
-  role       = "${aws_iam_role.beekeeper_path_scheduler_ecs_task.id[0]}"
+  role       = "${aws_iam_role.beekeeper_path_scheduler_ecs_task.*.id[0]}"
   policy_arn = "${aws_iam_policy.beekeeper_ecs_task_exec_docker_registry.*.arn[0]}"
 }
 
@@ -62,13 +62,13 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "beekeeper_path_scheduler_secrets" {
   count      = "${var.instance_type == "ecs" ? 1 : 0}"
-  role       = "${aws_iam_role.beekeeper_path_scheduler_ecs_task.id[0]}"
+  role       = "${aws_iam_role.beekeeper_path_scheduler_ecs_task.*.id[0]}"
   policy_arn = "${aws_iam_policy.beekeeper_secrets.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "beekeeper_path_scheduler_sqs" {
   count      = "${var.instance_type == "ecs" ? 1 : 0}"
-  role       = "${aws_iam_role.beekeeper_path_scheduler_ecs_task.id[0]}"
+  role       = "${aws_iam_role.beekeeper_path_scheduler_ecs_task.*.id[0]}"
   policy_arn = "${aws_iam_policy.beekeeper_sqs.arn}"
 }
 
@@ -96,12 +96,12 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "beekeeper_cleanup_secrets" {
   count      = "${var.instance_type == "ecs" ? 1 : 0}"
-  role       = "${aws_iam_role.beekeeper_cleanup_ecs_task.id[0]}"
+  role       = "${aws_iam_role.beekeeper_cleanup_ecs_task.*.id[0]}"
   policy_arn = "${aws_iam_policy.beekeeper_secrets.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "beekeeper_cleanup_s3" {
   count      = "${var.instance_type == "ecs" ? 1 : 0}"
-  role       = "${aws_iam_role.beekeeper_cleanup_ecs_task.id[0]}"
+  role       = "${aws_iam_role.beekeeper_cleanup_ecs_task.*.id[0]}"
   policy_arn = "${aws_iam_policy.beekeeper_s3.arn}"
 }
