d0f9d4649e1d45bb59f12c310c64ab402f5e35c9
John Lane <a-jolane@hotels.com>
2019-12-04 12:52:27 +0000

    revert

--- lambda/common.tf
+++ lambda/common.tf
@@ -6,7 +6,7 @@
 
 locals {
   instance_alias = "${ var.instance_name == "" ? "beekeeper" : format("beekeeper-%s",var.instance_name) }"
-  instance_alias = "${var.existing_instance_name != "" ? var.existing_instance_name : local.instance_alias}"
+  #instance_alias = "${var.existing_instance_name != "" ? var.existing_instance_name : local.instance_alias}"
 ## to accommodate existing long db name eg. bdp-beekeeper-hcom-data-lab
 
 }
