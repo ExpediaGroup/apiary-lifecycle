80dff5c66573c17ff2cc674bdf62df04e685949d
Scott Barnhart <sbarnhart@expedia.com>
2021-04-01 12:13:34 -0700

    use new required syntax

--- version.tf
+++ version.tf
@@ -7,10 +7,7 @@
 terraform {
   required_version = "> 0.12.0"
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 1.0"
-    }
+    kubernetes = "~> 1.0"
   }
 }
 
