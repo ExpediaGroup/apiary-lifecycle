147dcc36418326c7ce87669eabee15a26031c175
shermosa <shermosa@hotels.com>
2021-07-14 16:20:36 +0100

    snapshot

--- variables.tf
+++ variables.tf
@@ -240,7 +240,7 @@ variable "api_docker_image" {
 variable "api_docker_image_version" {
   description = "Beekeeper API docker image version."
   type        = string
-  default     = "3.2.0-SNAPSHOT"
+  default     = "latest"
 }
 
 # Application specific
