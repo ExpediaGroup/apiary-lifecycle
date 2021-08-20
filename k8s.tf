779cabfaf456f270289bb8c5d9d6215498cb5127
shermosa <shermosa@hotels.com>
2021-08-20 11:55:03 +0100

    add ingres

--- k8s.tf
+++ k8s.tf
@@ -12,3 +12,58 @@ locals {
   }
 }
 
+resource "kubernetes_ingress" "beekeeper" {
+  count = var.instance_type == "k8s" && var.k8s_ingress_enabled == 1 ? 1 : 0
+  metadata {
+    name        = local.k8s_app_alias
+    labels      = local.labels
+    annotations = {}
+    namespace = var.k8s_namespace
+  }
+
+  spec {
+    tls {
+      hosts       = var.k8s_ingress_tls_hosts
+      secret_name = var.k8s_ingress_tls_secret
+    }
+
+    rule {
+      host = var.k8s_path_cleanup_ingress_host
+      http {
+        path {
+          path = var.k8s_path_cleanup_ingress_path
+          backend {
+            service_name = kubernetes_service.beekeeper_path_cleanup[count.index].metadata.name
+            service_port = kubernetes_service.beekeeper_path_cleanup[count.index].spec.port.target_port
+          }
+        }
+      }
+    }
+
+    rule {
+      host = "${local.metadata_cleanup_full_name}.${local.dnsname}.${local.dnsdomain}"
+      http {
+        path {
+          path = var.k8s_metadata_cleanup_ingress_path
+          backend {
+            service_name = kubernetes_service.beekeeper_metadata_cleanup[count.index].metadata.name
+            service_port = kubernetes_service.beekeeper_metadata_cleanup[count.index].spec.port.target_port
+          }
+        }
+      }
+    }
+
+    rule {
+      host = var.k8s_scheduler_apiary_ingress_host
+      http {
+        path {
+          path = var.k8s_scheduler_apiary_ingress_path
+          backend {
+            service_name = kubernetes_service.beekeeper_scheduler_apiary[count.index].metadata.name
+            service_port = kubernetes_service.beekeeper_scheduler_apiary[count.index].spec.port.target_port
+          }
+        }
+      }
+    }
+  }
+}
