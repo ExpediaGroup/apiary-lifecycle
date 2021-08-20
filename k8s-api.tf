20c9e90cb7cafcad86393538b0ddd2f5bb42b0d6
shermosa <shermosa@hotels.com>
2021-08-20 11:28:48 +0100

    remove namspace

--- k8s-api.tf
+++ k8s-api.tf
@@ -132,7 +132,6 @@ resource "kubernetes_service" "beekeeper_api" {
 resource "kubernetes_ingress" "beekeeper-api" {
   metadata {
     name = local.api_full_name
-    namespace = var.k8s_namespace
   }
 
   spec {
