00fa98937aa53130731b194c32ad3880e3fae084
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-11-15 18:56:46 -0800

    feat: update to ingress_v1

--- k8s.tf
+++ k8s.tf
@@ -33,8 +33,10 @@ resource "kubernetes_ingress_v1" "beekeeper" {
         path {
           path = var.k8s_path_cleanup_ingress_path
           backend {
-            service_name = kubernetes_service.beekeeper_path_cleanup[0].metadata[0].name
-            service_port = kubernetes_service.beekeeper_path_cleanup[0].spec[0].port[0].target_port
+            name = kubernetes_service.beekeeper_path_cleanup[0].metadata[0].name
+            port {
+                number = kubernetes_service.beekeeper_path_cleanup[0].spec[0].port[0].target_port
+            }
           }
         }
       }
@@ -46,8 +48,10 @@ resource "kubernetes_ingress_v1" "beekeeper" {
         path {
           path = var.k8s_metadata_cleanup_ingress_path
           backend {
-            service_name = kubernetes_service.beekeeper_metadata_cleanup[0].metadata[0].name
-            service_port = kubernetes_service.beekeeper_metadata_cleanup[0].spec[0].port[0].target_port
+            name = kubernetes_service.beekeeper_metadata_cleanup[0].metadata[0].name
+            port {
+                number = kubernetes_service.beekeeper_metadata_cleanup[0].spec[0].port[0].target_port
+            }
           }
         }
       }
@@ -59,8 +63,10 @@ resource "kubernetes_ingress_v1" "beekeeper" {
         path {
           path = var.k8s_scheduler_apiary_ingress_path
           backend {
-            service_name = kubernetes_service.beekeeper_scheduler_apiary[0].metadata[0].name
-            service_port = kubernetes_service.beekeeper_scheduler_apiary[0].spec[0].port[0].target_port
+            name = kubernetes_service.beekeeper_scheduler_apiary[0].metadata[0].name
+            port {
+                number = kubernetes_service.beekeeper_scheduler_apiary[0].spec[0].port[0].target_port
+            }
           }
         }
       }
