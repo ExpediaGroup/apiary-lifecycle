ba80ae5b52250094e8de24ae9d0495581b296d38
githubjianli <51385385+githubjianli@users.noreply.github.com>
2023-11-15 18:46:35 -0800

    feat: kubernetes_deployment_v1

--- k8s-path-cleanup.tf
+++ k8s-path-cleanup.tf
@@ -19,7 +19,7 @@ locals {
   }
 }
 
-resource "kubernetes_deployment" "beekeeper_path_cleanup" {
+resource "kubernetes_deployment_v1" "beekeeper_path_cleanup" {
   count = var.instance_type == "k8s" ? 1 : 0
   metadata {
     name      = local.path_cleanup_full_name
