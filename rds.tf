fa955a316dcbbdaded92f7f483e4b3ad4bebe2ee
paduin <a-paduin@expediagroup.com>
2024-08-21 14:25:14 +0200

    test

--- rds.tf
+++ rds.tf
@@ -67,6 +67,6 @@ resource "aws_db_instance" "beekeeper" {
   apply_immediately            = var.db_apply_immediately
   performance_insights_enabled = var.db_performance_insights_enabled
   final_snapshot_identifier    = "${local.instance_alias}-final-snapshot-${random_id.snapshot_id.hex}"
-  tags                         = var.beekeeper_tags
   copy_tags_to_snapshot        = var.db_copy_tags_to_snapshot
+  tags                         = var.beekeeper_tags
 }
